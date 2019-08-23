396a4e39bd201e9c97cdbe30038f09c8fa6a05f9
Joel Adams <joeladamsdesign@gmail.com>
2019-08-23 14:24:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App!</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'It work?'));
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!</h1>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'It work?'));
   }
 }
 
