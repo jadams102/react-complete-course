ab422978b5bef17dc0efefec79f4559c9f5df634
Joel Adams <joeladamsdesign@gmail.com>
2019-08-23 16:49:49 -0700

    update for dynamic content

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // DONT DO THIS this.state.persons[0].name = "Joeliosis";
     this.setState({
       persons: [
-        { name: 'Joeliosis', age: 30 },
+        { name: newName, age: 30 },
         { name: 'Morriganism', age: 21 },
         { name: 'Jackie Boi', age: 27 }
       ]
@@ -28,10 +28,17 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>THIS IS REALLY WORKING</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies include being rad as fuck</Person>
+        <button onClick={this.switchNameHandler.bind(this, 'Joeliosism')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Joeliosism!')}/>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>My Hobbies include being rad as fuck</Person>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'It work?'));
