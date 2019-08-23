e5a7077d0176d50581fa39b4cad78d259b0e4ca8
Joel Adams <joeladamsdesign@gmail.com>
2019-08-23 16:03:11 -0700

    update for use of props

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>THIS IS REALLY WORKING</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Joel" age="30" />
+        <Person name="Morrigan" age="21"/>
+        <Person name="Jack" age="27">My Hobbies include being rad as fuck</Person>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'It work?'));
