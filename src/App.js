371308c3a1df6064aa915580cf5a01a920c5faf0
Joel Adams <joeladamsdesign@gmail.com>
2019-08-23 16:23:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ class App extends Component {
         <h1>Hi, I'm a React App!</h1>
         <p>THIS IS REALLY WORKING</p>
         <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age="30" />
-        <Person name="Morrigan" age="21"/>
-        <Person name="Jack" age="27">My Hobbies include being rad as fuck</Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies include being rad as fuck</Person>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'It work?'));
