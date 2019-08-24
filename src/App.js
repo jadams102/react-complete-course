210370ba98089d89c0b4a8e10fefc0f146c62e72
Joel Adams <joeladamsdesign@gmail.com>
2019-08-23 17:19:19 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
-      border: '1x solid blue',
+      border: '1px solid blue',
       padding: '8px'
     }
 
