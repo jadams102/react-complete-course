831d7a3284332b5d3539c494849b865e72b998b7
Joel Adams <joeladamsdesign@gmail.com>
2019-08-26 19:58:17 -0700

    update class errors

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ class App extends Component {
   render() {
 
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showpersons) {
       persons = (
@@ -61,6 +62,8 @@ class App extends Component {
           })}
         </div>
       );
+
+      btnClass = classes.red
 
     }
 
@@ -79,6 +82,7 @@ class App extends Component {
           <h1>Hi, I'm a React App!</h1>
           <p className={assignedClasses.join(' ')}>THIS IS REALLY WORKING</p>
           <button
+            className={btnClass}
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
             {persons}
         </div>
