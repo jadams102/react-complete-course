f707b88144e0219ed201344913d2c1c961e50c87
Joel Adams <joeladamsdesign@gmail.com>
2019-08-26 19:07:47 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,7 +53,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover':{
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -73,6 +77,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'white'
+      }
     }
 
     let classes = [];
