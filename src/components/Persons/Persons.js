18927f3c155fb7d3f61f0b65a2295454fe5ef3fa
Joel Adams <joeladamsdesign@gmail.com>
2019-08-27 09:26:42 -0700

    Update Persons.js

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,1 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Person from './Person/Person';
+
+const persons = (props) => (
+    props.persons.map((person, index) => {
+        return <Person
+            key={person.id}
+            click={() => this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age}
+            changed={(event) => this.nameChangedHandler(event, person.id)} />
+      })
+);
