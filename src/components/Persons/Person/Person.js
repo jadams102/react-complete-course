3e35c45d60a341af2775192b94f030b6219734b0
Joel Adams <joeladamsdesign@gmail.com>
2019-08-27 15:44:35 -0700

    Update Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -8,16 +8,12 @@ class Person extends Component {
         console.log('[Person.js] render')
         return (
             <Aux>
-                <p key="1l" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
-            <p key="2l">{this.props.children}</p>,
-            <input key="3l" type="text" onChange={this.props.changed} value={this.props.name} />
+                <p key="1l" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p key="2l">{this.props.children}</p>
+                <input key="3l" type="text" onChange={this.props.changed} value={this.props.name} />
             </Aux>
         )
-
-
-
     }
-
 };
 
 export default Person;
