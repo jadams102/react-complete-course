bb79bd3da9e462ab0a9b8a636a8af2e63a9333f1
Joel Adams <joeladamsdesign@gmail.com>
2019-08-27 09:35:36 -0700

    Update Cockpit.js

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const cockpit = (props) => {
     return (
-
+        <div>
+            <h1>Hi, I'm a React App!</h1>
+            <p className={assignedClasses.join(' ')}>THIS IS REALLY WORKING</p>
+            <button
+                className={btnClass}
+                onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        </div>
     );
 };
 
