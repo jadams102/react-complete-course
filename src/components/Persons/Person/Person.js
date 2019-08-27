3388ee62e93b908deac6b0d6ffced37cdcd9da7b
Joel Adams <joeladamsdesign@gmail.com>
2019-08-27 15:41:15 -0700

    add aux component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+
+import Aux from '../../../hoc/Aux'
 import classes from './Person.css';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] render')
         return (
-            <div className={classes.Person}>
-            <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-            <p>{this.props.children}</p>
-            <input type="text" onChange={this.props.changed} value={this.props.name}/>
-        </div>
+            <Aux>
+                <p key="1l" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+            <p key="2l">{this.props.children}</p>,
+            <input key="3l" type="text" onChange={this.props.changed} value={this.props.name} />
+            </Aux>
         )
+
+
+
     }
- 
+
 };
 
 export default Person;
