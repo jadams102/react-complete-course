6542130408a53440495de8a510364bade50cc2b0
Joel Adams <joeladamsdesign@gmail.com>
2019-08-27 16:55:13 -0700

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends Component {
       { id: 'sfasf', name: 'Jack', age: 27 }
     ],
     showpersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -55,7 +56,10 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => {
+      return { persons: persons, changeCounter: prevState.changeCounter +1 }
+
+    });
   }
 
   deletePersonHandler = (personIndex) => {
