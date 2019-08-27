f36bba085923089b7fa64aa9614e4fdda3f64fed
Joel Adams <joeladamsdesign@gmail.com>
2019-08-27 11:46:47 -0700

    update persons.js

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => (
-    props.persons.map((person, index) => {
+const persons = (props) => {
+    console.log('[Persons.js] rendering...')
+
+    return props.persons.map((person, index) => {
         return <Person
             key={person.id}
             click={() => props.clicked(index)}
@@ -10,6 +12,6 @@ const persons = (props) => (
             age={person.age}
             changed={(event) => props.changed(event, person.id)} />
       })
-);
+    };
 
 export default persons;
