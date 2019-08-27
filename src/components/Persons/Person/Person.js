129b51c91f930ed605e34c0b0d42dfe895015625
Joel Adams <joeladamsdesign@gmail.com>
2019-08-27 16:33:02 -0700

    Update Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Auxiliary'
+import Aux from '../../../hoc/Auxiliary';
+import withClass from '../../../hoc/withClass'
 import classes from './Person.css';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] render')
         return (
-            <React.Fragment>
+            <Aux>
                 <p key="1l" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p key="2l">{this.props.children}</p>
                 <input key="3l" type="text" onChange={this.props.changed} value={this.props.name} />
-            </React.Fragment>
+            </Aux>
         )
     }
 };
 
-export default Person;
+export default withClass(Person, classes.Person);
