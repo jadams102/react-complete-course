bde27587bfa4ad230754034d3124977df879e995
Joel Adams <joeladamsdesign@gmail.com>
2019-08-27 17:39:34 -0700

    Update Cockpit.js

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
-import classes from './Cockpit.css'
+import classes from './Cockpit.css';
+import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
     const toggleBtnRef = useRef(null);
@@ -51,7 +52,9 @@ const cockpit = (props) => {
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons
             </button>
-            <button onClick={props.login}>Log In</button>
+            <AuthContext.Consumer>
+                {(context) => <button onClick={context.login}>Log In</button>}
+            </AuthContext.Consumer>
         </div>
     );
 };
